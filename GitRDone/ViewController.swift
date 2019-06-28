170b79e4f6783e5f01e9451939f66fa605225000
Carlson, Natalie <ncarlson@ltl00431.local>
2019-06-28 09:28:03 -0500

    This commit is to show the dev commit.

--- GitRDone/ViewController.swift
+++ GitRDone/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 // This is the class I was looking forward to all week.
 
+// Tom is the bomb
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        //  Explain the difference between load and appear.
     }
 
 }
